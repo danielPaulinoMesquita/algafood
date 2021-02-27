6605ac8de73c08581f49166b538ca498d4aa1ce9
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2021-02-27 20:24:59 -0300

    4- Implementando RestauranteRepository e RestauranteRepositoryImpl e adicionando dados no import.sql

---
+++ src/main/java/com/algaworks/algaworkscurso/domain/repository/RestauranteRepository.java
@@ -0,0 +1,14 @@
+package com.algaworks.algaworkscurso.domain.repository;
+
+import com.algaworks.algaworkscurso.domain.model.Restaurante;
+
+import java.util.List;
+
+public interface RestauranteRepository {
+
+    List<Restaurante> listar();
+    Restaurante buscar(Long id);
+    Restaurante salvar(Restaurante cozinha);
+    void remover(Restaurante cozinha);
+
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,6 @@
 INSERT INTO cozinha (nome) values ('Chinesa');
 INSERT INTO cozinha (nome) values ('Tailandesa');
+INSERT INTO restaurante (nome, taxa_frete) values ('Restaurante Jaackin',17.90);
+INSERT INTO restaurante (nome, taxa_frete) values ('MacDonald',20.90);
 
 
