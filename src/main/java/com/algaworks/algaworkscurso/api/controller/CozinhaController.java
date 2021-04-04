879549c5a1b9df4efa82abc5e1ba4e81a0fc6bf6
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2021-04-04 00:38:04 -0300

    11-  Customizando a representação em XML com Wrapper e anotações do Jackson

--- src/main/java/com/algaworks/algaworkscurso/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algaworkscurso/api/controller/CozinhaController.java
@@ -1,13 +1,16 @@
 package com.algaworks.algaworkscurso.api.controller;
 
+import com.algaworks.algaworkscurso.api.model.CozinhasXmlWrapper;
 import com.algaworks.algaworkscurso.domain.model.Cozinha;
 import com.algaworks.algaworkscurso.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.*;
 import java.util.List;
 
 @RestController
@@ -20,6 +23,11 @@ public class CozinhaController {
     @GetMapping
     public List<Cozinha> listar(){
         return cozinhaRepository.listar();
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+    public CozinhasXmlWrapper listarXml(){
+        return new CozinhasXmlWrapper(cozinhaRepository.listar());
     }
 
     @GetMapping("/{cozinhaId}")
--- src/main/java/com/algaworks/algaworkscurso/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algaworkscurso/domain/model/Cozinha.java
@@ -2,19 +2,19 @@ package com.algaworks.algaworkscurso.domain.model;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
-import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@JsonRootName("cozzinha")
 @Entity
 public class Cozinha {
 
@@ -23,7 +23,6 @@ public class Cozinha {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @JsonIgnore
     private String nome;
 
 }
