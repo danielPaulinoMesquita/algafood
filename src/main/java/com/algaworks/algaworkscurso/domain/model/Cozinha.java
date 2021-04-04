1565e16b794628429c78d126772408310310e343
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2021-04-03 23:11:27 -0300

    10- Adicionando anotações para representação Json e Xml.

--- src/main/java/com/algaworks/algaworkscurso/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algaworkscurso/domain/model/Cozinha.java
@@ -1,6 +1,9 @@
 package com.algaworks.algaworkscurso.domain.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,6 +14,7 @@ import java.util.Objects;
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@JsonRootName("cozzinha")
 @Entity
 public class Cozinha {
 
@@ -18,6 +22,8 @@ public class Cozinha {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @JsonIgnore
     private String nome;
 
 }
