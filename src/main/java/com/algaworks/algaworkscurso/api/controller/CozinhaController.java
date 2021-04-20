0a33301d73b66bfb2b1a5175cf9010c3a2519023
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2021-04-20 00:14:13 -0300

    15-  Criando metodos de adicionar e atuallizar cozinha.

--- src/main/java/com/algaworks/algaworkscurso/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algaworkscurso/api/controller/CozinhaController.java
@@ -3,14 +3,13 @@ package com.algaworks.algaworkscurso.api.controller;
 import com.algaworks.algaworkscurso.api.model.CozinhasXmlWrapper;
 import com.algaworks.algaworkscurso.domain.model.Cozinha;
 import com.algaworks.algaworkscurso.domain.repository.CozinhaRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.*;
 import java.util.List;
 
 @RestController
@@ -45,5 +44,23 @@ public class CozinhaController {
 
         return ResponseEntity.notFound().build();
 
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cozinha adicionar(@RequestBody Cozinha cozinha){
+        return cozinhaRepository.salvar(cozinha);
+    }
+
+    @PutMapping("/{cozinhaId}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha) {
+        Cozinha cozinhaAtual = cozinhaRepository.buscar(cozinhaId);
+        if (cozinhaAtual != null){
+            BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+            cozinhaRepository.salvar(cozinhaAtual);
+            return ResponseEntity.ok(cozinhaAtual);
+        }
+        return ResponseEntity.notFound().build();
     }
 }
