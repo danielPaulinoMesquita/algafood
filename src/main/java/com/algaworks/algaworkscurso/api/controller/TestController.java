b2ef10f8e7c4cce25d7a70c4cefcb227a579123b
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2021-05-18 21:59:44 -0300

    24.1- EXTRA Criando metodo de arquivos zipados.

--- src/main/java/com/algaworks/algaworkscurso/api/controller/TestController.java
+++ src/main/java/com/algaworks/algaworkscurso/api/controller/TestController.java
@@ -1,12 +1,22 @@
 package com.algaworks.algaworkscurso.api.controller;
 
 import com.algaworks.algaworkscurso.domain.model.Cozinha;
-import com.algaworks.algaworkscurso.domain.repository.CozinhaRepository;
+import com.algaworks.algaworkscurso.domain.service.CadastroCozinhaService;
 import com.algaworks.algaworkscurso.infrastructure.repository.CozinhaRepositoryImpl;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @RestController
 @RequestMapping("/teste")
@@ -15,10 +25,44 @@ public class TestController {
     @Autowired
     private CozinhaRepositoryImpl cozinhaRepository;
 
+    @Autowired
+    private CadastroCozinhaService service;
+
     /*Você pode pegar uma paramentro por QueryString ou QueryParam utilizando o @RequestParam
     * exemplo de uri com queryParam:  */
     @GetMapping("/cozinhas/por-nome")
     public List<Cozinha> cozinhasPorNome(@RequestParam String nome){
         return cozinhaRepository.buscarPorNome(nome);
+    }
+
+
+//    @RequestMapping(value="/zip/{nomeArquivo}", produces="application/zip", method = RequestMethod.GET)
+    @GetMapping(value="/zip/{nomeArquivo}", produces="application/zip")
+    public void zipFiles(HttpServletResponse response, @PathVariable String nomeArquivo) throws IOException {
+
+        //setting headers
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.addHeader("Content-Disposition", "attachment; filename=\"teste.zip\"");
+
+        ZipOutputStream zipOutputStream = new ZipOutputStream(response.getOutputStream());
+
+        // create a list to add files to be zipped
+        List<File> files = new ArrayList<>(2);
+        files = service.download(nomeArquivo);
+        //files.add(new File("README.md"));
+
+        // package files
+        for (File file : files) {
+            //new zip entry and copying inputstream with file to zipOutputStream, after all closing streams
+            zipOutputStream.putNextEntry(new ZipEntry(file.getName()));
+            FileInputStream fileInputStream = new FileInputStream(file);
+
+            IOUtils.copy(fileInputStream, zipOutputStream);
+
+            fileInputStream.close();
+            zipOutputStream.closeEntry();
+        }
+
+        zipOutputStream.close();
     }
 }
--- src/main/java/com/algaworks/algaworkscurso/domain/service/CadastroCozinhaService.java
+++ src/main/java/com/algaworks/algaworkscurso/domain/service/CadastroCozinhaService.java
@@ -9,6 +9,15 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @Service
 public class CadastroCozinhaService {
 
@@ -27,5 +36,33 @@ public class CadastroCozinhaService {
         }catch (DataIntegrityViolationException e){
             throw  new EntidadeEmUsoException(String.format("Cozinha de codigo %d n\u00E3o pode ser removida, pois est\u00E1 em uso", id));
         }
+    }
+
+    public List<File> download(String nome) throws IOException {
+        Path path = Paths.get("/tmp/teste/");
+        List<Path> paths = findByFileName(path, nome);
+        List<File> files=  new ArrayList<>();
+        for (Path path1: paths){
+            files.add(path1.toFile());
+        }
+        return files;
+    }
+
+    public static List<Path> findByFileName(Path path, String fileName)
+            throws IOException {
+
+        List<Path> result;
+        try (Stream<Path> pathStream = Files.find(path,
+                Integer.MAX_VALUE,
+                (p, basicFileAttributes) ->{
+                    if(Files.isDirectory(p) || !Files.isReadable(p)){
+                        return false;
+                    }
+                    return p.getFileName().toString().contains(fileName);
+                })
+        ) {
+            result = pathStream.collect(Collectors.toList());
+        }
+        return result;
     }
 }
