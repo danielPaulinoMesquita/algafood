e724c4b819426ad5ccc49003098536a933d4615a
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2021-06-19 21:05:30 -0300

    33- Consulta Simples usando Criteria Query e TypedQuery com jpql.

--- src/main/java/com/algaworks/algaworkscurso/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algaworkscurso/domain/model/Restaurante.java
@@ -1,12 +1,9 @@
 package com.algaworks.algaworkscurso.domain.model;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
-import java.util.Objects;
 
 @Data
 @Entity
